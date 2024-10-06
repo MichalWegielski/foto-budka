251b1589b12cf96bb4000faaad3ca0da7ec84439
Michal <hardcorpl5@gmail.com>
2024-10-06 22:38:55 +0200

    Main structure is ready

---
+++ foto-budka/src/pages/HomePage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import HomeScreen from '../components/HomeScreen'
+import AboutUsScreen from '../components/AboutUsScreen'
+import ContactScreen from '../components/ContactScreen'
+import Footer from '../components/Footer'
+
+const HomePage = () => {
+  return (
+    <>
+      <HomeScreen/>
+      <AboutUsScreen/>
+      <ContactScreen/>
+      <Footer/>
+    </>
+  )
+}
+
+export default HomePage
